feef351c9840ea21392bc87d5c6922642d22a73e
Nathaniel Nutter <iam@nnutter.com>
2018-11-28 22:58:23 -0600

    Update main.go

--- main.go
+++ main.go
@@ -21,15 +21,16 @@ func withFile(name string, flag int, perm os.FileMode, do func(f *os.File) error
 	return nil
 }
 
-const contactSize = 500
+const contactSize = 250
 
 func genSuffixSets(areaCode, prefix int16) [][]string {
-	xss := make([][]string, 0, 10)
+	xss := make([][]string, 1, 10000/contactSize+1)
 	xs := make([]string, 0, contactSize)
 	for i := 0; i <= 9999; i++ {
 		xs = append(xs, fmt.Sprintf("(%03d) %03d-%04d", areaCode, prefix, i))
 		if len(xs) == cap(xs) {
 			xss = append(xss, xs)
+			xss[0] = append(xss[0], xs[0])
 			xs = make([]string, 0, contactSize)
 		}
 	}
@@ -46,14 +47,17 @@ func main() {
 	}
 
 	suffixSets := genSuffixSets(areaCode, prefix)
-	for _, suffixes := range suffixSets {
-		fname := fmt.Sprintf("%s.vcf", suffixes[0])
-		err := withFile(fname, os.O_RDWR|os.O_CREATE, 0755, func(f *os.File) error {
-			return tmpl.Execute(f, suffixes)
-		})
-		if err != nil {
-			log.Fatal(err)
+	fname := fmt.Sprintf("%s.vcf", suffixSets[0][0])
+	err = withFile(fname, os.O_RDWR|os.O_CREATE, 0755, func(f *os.File) error {
+		for _, suffixes := range suffixSets {
+			if err := tmpl.Execute(f, suffixes); err != nil {
+				return err
+			}
 		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -65,4 +69,5 @@ FN:{{ index . 0 }}
 ORG:{{ index . 0 }};
 {{range .}}{{printf "TEL;type=OTHER;type=VOICE:%s\n" .}}{{end -}}
 X-ABShowAs:COMPANY
-END:VCARD`
+END:VCARD
+`
